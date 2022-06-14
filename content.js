15e96fd07f527e51cc8fe67b53c65835dcf2b1a4
thymelimne <91765107+thymelimne@users.noreply.github.com>
2022-06-14 11:33:49 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,3 @@
 /*
 This content file is written in No-JS, because it has no JS.
-
-(Comment now obselete)
 */
-
-var tag=document.createElement("input");
-var text = document.createTextNode("This is some new text.");
-var element = document.getElementById("new");
-element.appendChild(tag);
