01195c78698e19002e2f631335a92ff01f9a3684
thymelimne <91765107+thymelimne@users.noreply.github.com>
2022-06-14 14:19:09 -0400

    Create content.js

---
+++ content.js
@@ -0,0 +1,10 @@
+/*
+This content file is written in No-JS, because it has no JS.
+
+(Comment now obselete)
+*/
+
+var tag=document.createElement("input");
+var text = document.createTextNode("This is some new text.");
+var element = document.getElementById("new");
+element.appendChild(tag);
